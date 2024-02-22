f4963e48828594d19764bd91cdab704cdb40e90a
Tobias <tobias@sernhede.com>
2024-02-22 14:32:16 +0100

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,8 +4,11 @@ import type { Reroute } from '@sveltejs/kit';
 export const reroute: Reroute = ({ url }) => {
     console.log("## REROUTE ##");
     console.log(url.pathname)
-    if (url.pathname === '/isr') {
+    if (url.pathname === '/isr' || url.pathname === '/isr/__data.json' ) {
         console.log("HERE")
+        if(url.pathname === '/isr/__data.json') {
+            return '/isr-rerouted/__data.json'
+        }
         return '/isr-rerouted'
     }
 };
