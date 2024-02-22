e71d04ef512f772640ba0317143d5ffc7aedccab
Tobias <tobias@sernhede.com>
2024-02-22 21:49:29 +0100

    updates

--- src/hooks.ts
+++ src/hooks.ts
@@ -17,18 +17,6 @@ export const reroute: Reroute = ({ url }) => {
         return reroutes[url.pathname];
     }
 
-    // if (url.pathname.startsWith('/isr')) {
-    //     return '/reroute-and-isr' + url.pathname.replace('/isr', '');
-    // }
-
-    // if (url.pathname.startsWith('/reroute/no-isr')) {
-    //     return '/reroute-without-isr' + url.pathname.replace('/reroute/no-isr', '');
-    // }
-
-    // if (url.pathname.startsWith('/no-ssr')) {
-    //     return '/reroute-without-ssr' + url.pathname.replace('/no-ssr', '');
-    // }
-
     console.log("Not rerouted");
 }
 
