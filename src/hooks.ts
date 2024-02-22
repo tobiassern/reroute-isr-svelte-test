daa3cadcaf91da0513e9c215ca0e857b78a3d941
Tobias <tobias@sernhede.com>
2024-02-22 14:54:46 +0100

    Update hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,9 @@
 import type { Reroute } from '@sveltejs/kit';
 
-const translated: { [n: string]: string } = {
-    '/isr': '/isr-rerouted',
-};
-
 export const reroute: Reroute = ({ url }) => {
-    console.log(`REROUTE`, url.toString());
 
-    let suffix = '';
-    let pathname = url.pathname;
-    const segments = pathname.split('/');
-    const lastSegment = segments.at(-1);
-    if (lastSegment && /\.\w+$/.test(lastSegment)) {
-        suffix = '/' + lastSegment;
-        pathname = segments.slice(0, -1).join('/');
-    }
-
-    if (pathname in translated) {
-        return translated[pathname] + suffix;
+    if (url.pathname === '/isr') {
+        return '/isr-rerouted'
     }
 }
 
